6fae9d9789ca40f27239b903bdc4afaf0ce46fb1
Arnau Sanchez <pyarnau@gmail.com>
2024-08-11 16:15:34 +0200

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+/** @format */
+
+module.exports = {
+    printWidth: 80,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: "always",
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    proseWrap: "preserve",
+    requirePragma: false,
+    insertPragma: false,
+};
